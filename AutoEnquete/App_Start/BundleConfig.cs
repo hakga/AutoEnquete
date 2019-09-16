d6f44e108b43e5759984417247854a0e5714912e
hakga <hakga.sone@gmail.com>
2019-09-16 10:04:24 +0900

    問題内容をタブ形式に対応※JSONデータ重要

--- AutoEnquete/App_Start/BundleConfig.cs
+++ AutoEnquete/App_Start/BundleConfig.cs
@@ -11,6 +11,9 @@ namespace AutoEnquete {
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+            "~/Scripts/knockout-{version}.js", "~/Scripts/knockout.validation.js", "~/Scripts/knockout.mapping-latest.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
